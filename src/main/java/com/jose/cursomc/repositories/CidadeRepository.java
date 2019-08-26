bff06866b18e24d7b4b43c424fe31adfb002ec7e
jose neves <josentt21@gmail.com>
2019-08-26 19:24:25 -0100

    EStado e Cidade

---
+++ src/main/java/com/jose/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,14 @@
+package com.jose.cursomc.repositories;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jose.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+	
+
+}
---
+++ src/main/java/com/jose/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,14 @@
+package com.jose.cursomc.repositories;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jose.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+	
+
+}
