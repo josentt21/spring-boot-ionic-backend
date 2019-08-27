d38e31ebc93429c29d593f3d4faa65043dbe6ed9
jose neves <josentt21@gmail.com>
2019-08-27 20:24:04 -0100

    Pedido, EstadoPagamento, Pagamento

---
+++ src/main/java/com/jose/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package com.jose.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.jose.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer>{
+	
+
+}
---
+++ src/main/java/com/jose/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,11 @@
+package com.jose.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.jose.cursomc.domain.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
+	
+
+}
